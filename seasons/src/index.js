00d432dca7631eb6cf98a8fec9ecc55cc6d9d651
Jack Davidson <jackd@fes.org>
2019-05-08 12:02:29 -0500

    Showing Latitude, Error, or Loading

---
+++ seasons/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Only time we directly assign the state. Otherwise call this.setState();
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
